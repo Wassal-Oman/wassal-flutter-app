9ac50d1c1cacd0c4f6a59cd033245d4a9d395cfa
Ghanim Al Marzouqi <ghanim.marzouqi@thawani.om>
2018-11-03 22:07:43 +0400

    trucker home page modified

--- lib/customer/customer_home.dart
+++ lib/customer/customer_home.dart
@@ -7,6 +7,27 @@ class CustomerHome extends StatelessWidget {
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(title: Text('Home')),
+      drawer: Drawer(
+        child: ListView(
+          padding: EdgeInsets.zero,
+          children: <Widget>[
+            DrawerHeader(
+              child: Text('Ghanim Al-Marzouqi', style: TextStyle(color: Colors.white),),
+              decoration: BoxDecoration(
+                color: Color.fromARGB(255, 190, 24, 34),
+              ),
+            ),
+            ListTile(
+              title: Text('Item 1'),
+              onTap: () {},
+            ),
+            ListTile(
+              title: Text('Item 2'),
+              onTap: () {},
+            ),
+          ],
+        ),
+      ),
       body: Center(
         child: Text('Customer Home Page'),
       ),
--- lib/trucker/trucker_home.dart
+++ lib/trucker/trucker_home.dart
@@ -1,12 +1,89 @@
 // import needed libraries
+import 'dart:async';
 import 'package:flutter/material.dart';
+import 'package:firebase_auth/firebase_auth.dart';
+import 'package:cloud_firestore/cloud_firestore.dart';
 
 // class widget
-class TruckerHome extends StatelessWidget {
+class TruckerHome extends StatefulWidget {
+  @override
+  State<StatefulWidget> createState() => _TruckerHome();
+}
+
+class _TruckerHome extends State<TruckerHome> {
+
+    // attributes
+  String name = 'User Name';
+  String email = 'User Email';
+
+  // firebase
+  FirebaseAuth auth;
+  FirebaseUser user;
+  Firestore database;
+
+  @override
+  void initState() {
+
+    auth = FirebaseAuth.instance;
+    database = Firestore.instance;
+
+    // get current user
+    getLoggedUser().then((FirebaseUser firebaseUser) {
+      if(firebaseUser != null) {
+        user = firebaseUser;
+
+        // get user details
+        getUserDetails(user.uid).then((DocumentSnapshot snapshot) {
+          name = snapshot.data['trucker_name'];
+          email = snapshot.data['trucker_email'];
+        }).catchError((error) {
+          print(error);
+        });
+      }
+    }).catchError((AuthException e) {
+      print(e.message);
+    });
+    super.initState();
+  }
+
+  Future<FirebaseUser> getLoggedUser() async {
+    return await auth.currentUser();
+  }
+
+  Future<DocumentSnapshot> getUserDetails(String id) async {
+    return await database.collection('truckers').document(id).get();
+  }
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(title: Text('Truck Driver Home'),),
+      drawer: Drawer(
+        child: ListView(
+          padding: EdgeInsets.zero,
+          children: <Widget>[
+            DrawerHeader(
+              child: Column(
+                children: <Widget>[
+                  Text(name, style: TextStyle(color: Colors.white),),
+                  Text(email, style: TextStyle(color: Colors.white),),
+                ],
+              ),
+              decoration: BoxDecoration(
+                color: Color.fromARGB(255, 190, 24, 34),
+              ),
+            ),
+            ListTile(
+              title: Text('Item 1'),
+              onTap: () {},
+            ),
+            ListTile(
+              title: Text('Item 2'),
+              onTap: () {},
+            ),
+          ],
+        ),
+      ),
       body: Center(
         child: Text('Welcome To Home Page'),
       ),
